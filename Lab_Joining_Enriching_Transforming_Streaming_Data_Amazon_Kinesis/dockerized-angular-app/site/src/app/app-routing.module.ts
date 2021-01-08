2d3c30bfc6e0d4e0fee9cb292f682cd83f0084a1
brocktubre <brocktubre@gmail.com>
2021-01-08 11:33:03 -0500

    changed landing page

--- Lab_Joining_Enriching_Transforming_Streaming_Data_Amazon_Kinesis/dockerized-angular-app/site/src/app/app-routing.module.ts
+++ Lab_Joining_Enriching_Transforming_Streaming_Data_Amazon_Kinesis/dockerized-angular-app/site/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: KinesisHelperComponent },
     { path: 'kinesis-helper', component: KinesisHelperComponent },
     { path: '**', redirectTo: '' }
     
